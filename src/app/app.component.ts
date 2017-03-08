d4c0cb27a5ad6952aa9440354c19b8c4492d12f8
florian25686 <florianvo@gmail.com>
2017-03-08 22:00:52 +0100

    build it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class HandImage {
+        id: number;
+        name: string;
+        side: string;
+        path: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <div>
+      <label>Bild1: <img src="{{image1.path}}"></label>
+    </div>
+    `
 })
+
 export class AppComponent {
-  title = 'app works!';
+	title = 'Spiegeln';
+	image1: HandImage = {
+        	id: 1,
+       		name: 'Bild2',
+       	 	side: 'right',
+        	path: 'src/img/bofh.png'
+        };
 }
+
