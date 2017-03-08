cdcbbbb09e5dcbe3e188922838d749ee73da159c
florian25686 <florianvo@gmail.com>
2017-03-08 22:30:23 +0100

    Fixed the application tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 
+export class HandImage {
+        id: number;
+        name: string;
+        side: string;
+        path: string;
+}
+
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
     <div>
+      <label>Bild1: <img src="{{image1.path}}"></label>
     </div>
     `
 })
 
 export class AppComponent {
-	title = 'Spiegeln';
+  title = 'Tour of Heroes';
+  image1: HandImage = {
+        id: 1,
+        name: 'Bild2',
+        side: 'right',
+        path: '/img/bofh.png'
+        };
 }
-
