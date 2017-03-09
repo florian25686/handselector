17eb7a36c4146801972a385fd4a3daea1a0c49b9
florian25686 <florianvo@gmail.com>
2017-03-09 12:25:07 +0100

    YEah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 
 export class HandImage {
-        id: number;
-        name: string;
-        side: string;
-        path: string;
+constructor(
+        public id: number,
+        public name: string,
+        public side: string,
+        public path: string) {}
 }
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <div>
+    <div id="images">
       <label>Bild1: <img src="{{image1.path}}"></label>
+	<ul>
+		<li *ngFor="let bild of existingImages">
+		name: {{bild.name}} <img src="{{bild.path}}">
+		</li>
+	</ul>
     </div>
     `
 })
@@ -23,6 +29,14 @@ export class AppComponent {
         id: 1,
         name: 'Bild2',
         side: 'right',
-        path: '/img/bofh.png'
+        path: 'src/img/bofh.png'
         };
+
+  existingImages = [
+	new HandImage(2,'Bild3', 'right', 'src/img/img2.jpg'),
+	new HandImage(3,'Bild4', 'left', '')
+	];
+
+  heroes = ['Windstorm', 'Bombasto', 'Magenta', 'Torando'];
+ myHery = this.heroes[0];
 }
