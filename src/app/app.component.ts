b9e34a603e35dcc3301517a5f65c73fca58d2f60
Florian Völker <florian.voelker@kyberna.com>
2017-03-09 15:36:46 +0100

    Corrected the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 
-export class HandImage {
-constructor(
-        public id: number,
-        public name: string,
-        public side: string,
-        public path: string) {}
-}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
     <div id="images">
-      <label>Bild1: <img src="{{image1.path}}"></label>
-    	<ul>
-    		<li *ngFor="let bild of existingImages" style="display:none;">
-    		  name: {{bild.name}} <img src="{{bild.path}}">
-    		</li>
-    	</ul>
+      <div><img src="{{image1.path}}"></div>
       <click-me></click-me>
     </div>
     `
@@ -33,11 +21,5 @@ export class AppComponent {
         path: 'src/img/bofh.png',
         test: 'hello'
         };
-  existingImages = [
-  	new HandImage(2,'Bild3', 'right', 'src/img/img2.jpg'),
-  	new HandImage(3,'Bild4', 'left', '')
-	];
 
-  heroes = ['Windstorm', 'Bombasto', 'Magenta', 'Torando'];
- myHery = this.heroes[0];
 }
