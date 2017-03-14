8e17e9e5206b58e47c090cf7eaf7531fbc944d4a
florian25686 <florianvo@gmail.com>
2017-03-14 10:28:31 +0100

    Fixed Image width and added new Images

--- src/app/images/mock-handimages.ts
+++ src/app/images/mock-handimages.ts
@@ -1,6 +1,6 @@
 import { HandImage } from './handImages';
 
-const IMAGEPATH = 'src/img/small/';
+const IMAGEPATH = 'src/img/';
 
 export var EXISTINGIMAGES: HandImage[] = [
   { id: 1, name: 'Bild1', side: 'left', path: IMAGEPATH+'img7.png'},
@@ -9,14 +9,14 @@ export var EXISTINGIMAGES: HandImage[] = [
   { id: 4, name: 'Bild4', side: 'left', path:  IMAGEPATH+'img8.png'},
   { id: 5, name: 'Bild5', side: 'right', path: IMAGEPATH+'img15.png'},
   { id: 6, name: 'Bild6', side: 'left', path: IMAGEPATH+'img17.png'},
-  { id: 7, name: 'Bild7', side: 'left', path: IMAGEPATH+'img19.png'},
+  { id: 7, name: 'Bild7', side: 'right', path: IMAGEPATH+'img19.png'},
   { id: 8, name: 'Bild8', side: 'right', path: IMAGEPATH+'img6.png'},
-  { id: 9, name: 'Bild9', side: 'right', path: IMAGEPATH+'img16.png'},
+  { id: 9, name: 'Bild9', side: 'left', path: IMAGEPATH+'img16.png'},
   { id: 10, name: 'Bild10', side: 'right', path: IMAGEPATH+'img4.png'},
-  { id: 11, name: 'Bild11', side: 'left', path: IMAGEPATH+'img18.png'},
+  { id: 11, name: 'Bild11', side: 'right', path: IMAGEPATH+'img18.png'},
   { id: 12, name: 'Bild12', side: 'right', path: IMAGEPATH+'img9.png'},
   { id: 13, name: 'Bild13', side: 'left', path: IMAGEPATH+'img13.png'},
-  { id: 14, name: 'Bild14', side: 'left', path: IMAGEPATH+'img14.png'},
+  { id: 14, name: 'Bild14', side: 'right', path: IMAGEPATH+'img14.png'},
   { id: 15, name: 'Bild15', side: 'right', path: IMAGEPATH+'img5.png'},
   { id: 16, name: 'Bild16', side: 'left', path: IMAGEPATH+'img20.png'},
   { id: 17, name: 'Bild17', side: 'right', path: IMAGEPATH+'img10.png'},
