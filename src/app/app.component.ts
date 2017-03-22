d20de73a5a405a7098574e264372bf3597748caa
florian25686 <florianvo@gmail.com>
2017-03-22 19:55:45 +0100

    Started to create a selection for images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <div id="testing">
-		<click-me></click-me>
+    <div id="ImageSelector" *ngIf="ImageSelector == 1">
+      <select id="imageTypeSelector" name="imageTypes">
+        <option value="hand">Hand Bilder</option>
+        <option value="feet">Fuss Bilder</option>
+      </select>
+    </div>
+    <div id="images" *ngIf="ImageSelector == 0">
+		  <click-me></click-me>
     </div>
     `
 })
 
 export class AppComponent {
   title = 'Spiegeltherapie';
+  ImageSelector = 1;
 }
